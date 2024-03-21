62a853c5f820a0d6e3724ed601b2b3747ec77db9
AkapmHos <akaphon.m@heliostech.team>
2024-03-22 00:55:29 +0700

    Admin ui

--- app/BookingForm/page.jsx
+++ app/BookingForm/page.jsx
@@ -22,18 +22,18 @@ import {
   TableRow,
   TableCell,
   User,
-  Tooltip
+  Tooltip,
 } from "@nextui-org/react";
 import { Tabs, Tab, CardBody } from "@nextui-org/react";
 import Loading from "@/app/loading";
 import useSWR from "swr";
-import {ApproveIcon} from "./ApproveIcon";
-import {DeleteIcon} from "./DeleteIcon";
-import {columns, users} from "./approvedata";
+import { ApproveIcon } from "./ApproveIcon";
+import { DeleteIcon } from "./DeleteIcon";
+import { columns, users } from "./approvedata";
 const statusColorMap = {
-  active: "success",
-  paused: "danger",
-  vacation: "warning",
+  approve: "success",
+  reject: "danger",
+  waiting: "warning",
 };
 
 // สมมติว่าคุณมีข้อมูลห้องพักและจำนวนคงเหลือดังนี้
@@ -58,7 +58,7 @@ export default function BookingPage() {
       case "name":
         return (
           <User
-            avatarProps={{radius: "lg", src: user.avatar}}
+            avatarProps={{ radius: "lg", src: user.avatar }}
             description={user.email}
             name={cellValue}
           >
@@ -69,20 +69,26 @@ export default function BookingPage() {
         return (
           <div className="flex flex-col">
             <p className="text-bold text-sm capitalize">{cellValue}</p>
-            <p className="text-bold text-sm capitalize text-default-400">{user.team}</p>
+            <p className="text-bold text-sm capitalize text-default-400">
+              {user.team}
+            </p>
           </div>
         );
       case "status":
         return (
-          <Chip className="capitalize" color={statusColorMap[user.status]} size="sm" variant="flat">
+          <Chip
+            className="capitalize"
+            color={statusColorMap[user.status]}
+            size="sm"
+            variant="flat"
+          >
             {cellValue}
           </Chip>
         );
       case "actions":
         return (
           <div className="relative flex items-center gap-2">
-            
-            <Tooltip content="Edit user">
+            <Tooltip content="Approve">
               <span className="text-lg text-default-400 cursor-pointer active:opacity-50">
                 <ApproveIcon />
               </span>
@@ -166,10 +172,6 @@ export default function BookingPage() {
   if (status === "loading") {
     return <Spinner color="success" />;
   }
-
-
-  
-  
 
   //เมื่อเข้าสู่ระบบเรียบร้อยแล้ว ข้อมูล data และ data user พร้อม
   if (status === "authenticated") {
@@ -282,24 +284,30 @@ export default function BookingPage() {
             </Card>
           </Tab>
           <Tab key="pending" title="Admin อนุมัติ">
-          <Table aria-label="Example table with custom cells">
-          <TableHeader columns={columns}>
-        {(column) => (
-          <TableColumn key={column.uid} align={column.uid === "actions" ? "center" : "start"}>
-            {column.name}
-          </TableColumn>
-        )}
-      </TableHeader>
-      <TableBody items={users}>
-        {(item) => (
-          <TableRow key={item.id}>
-            {(columnKey) => <TableCell>{renderCell(item, columnKey)}</TableCell>}
-          </TableRow>
-        )}
-      </TableBody>
+            <Table aria-label="Example table with custom cells">
+              <TableHeader columns={columns}>
+                {(column) => (
+                  <TableColumn                    
+                    key={column.uid}
+                    align={column.uid === "actions" ? "center" : "start"}
+                    className="text-center"
+                  >
+                    {column.name}
+                  </TableColumn>
+                )}
+              </TableHeader>
+              <TableBody items={users}>
+                {(item) => (
+                  <TableRow key={item.id}>
+                    {(columnKey) => (
+                      <TableCell>{renderCell(item, columnKey)}</TableCell>
+                    )}
+                  </TableRow>
+                )}
+              </TableBody>
             </Table>
           </Tab>
-          <Tab key="check-booking" title="รายการที่ลงทั้งหมด">
+          <Tab key="check-booking" title="รายการอนุมัติ">
             <Table aria-label="Example static collection table">
               <TableHeader>
                 <TableColumn className="text-center">Name</TableColumn>
