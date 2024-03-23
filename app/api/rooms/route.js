86e793df377749fdf901d81ea7e4b04e6b467e37
AkapmHos <akaphon.m@heliostech.team>
2024-03-24 00:55:28 +0700

    api room

--- app/api/rooms/route.js
+++ app/api/rooms/route.js
@@ -1,9 +1,7 @@
-import mongodbConnect from "../../../lib/mongodb";
 import fetchRooms from '../../../lib/mongodb';
 export async function GET(req, res) {
-  // const data = await mongodbConnect();
   const rooms = await fetchRooms();
-  // console.log("this2",rooms);
+  console.log("this2",rooms);
   return Response.json({
     rooms
   })
