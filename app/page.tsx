848d09151e567cc49d3251cb6d263d3a732f9cce
Akapm <akaphon.mahaphon@gmail.com>
2024-09-10 00:59:21 +0700

    save by gas

--- app/page.tsx
+++ app/page.tsx
@@ -27,7 +27,7 @@ export default async function Home() {
         <br />
         <h1 className={title()}>ได้อย่างง่ายดายและสะดวกรวดเร็ว</h1>
         <h2 className={subtitle({ class: "mt-4" })}>
-          ระบบจองEARTHห้องพักที่ใช้งานง่ายและมีประสิทธิภาพ
+          GAS
         </h2>
       </div>
 
